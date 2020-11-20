abe4b4eccf4946ed0e9cc160ff1256424a40e981
yue-cherry-ying <cherryying@uchicago.edu>
2020-11-20 21:44:05 +0000

    finished basics

---
+++ app/models/movie.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+  has_many(:characters)
+  has_many(:actors, :through => :characters, :source => :actors)
+  validates(:title, { :uniqueness => { :scope => ["year", "director_id"] } })
+  validates(:title, { :presence => true })
+end
